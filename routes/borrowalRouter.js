066440daa81af981eb05c72e8ae4d61c4d2ef468
zarmaull <zarmaull178@gmail.com>
2025-06-14 04:55:05 +0700

    Fix: Menyamakan casing BorrowalRouter dan controller, restore authController

--- routes/borrowalRouter.js
+++ routes/borrowalRouter.js
@@ -1,24 +1,51 @@
+// // Import required modules
+// const express = require("express")
+// const router = express.Router();
+
+// // Import functions from controller
+// const {
+//     getBorrowal,
+//     getAllBorrowals,
+//     addBorrowal,
+//     updateBorrowal,
+//     deleteBorrowal
+// } = require('../controllers/BorrowalController')
+
+// router.get("/getAll", (req, res) => getAllBorrowals(req,res))
+
+// router.get("/get/:id", (req, res) => getBorrowal(req, res))
+
+// router.post("/add", (req, res) => addBorrowal(req, res))
+
+// router.put("/update/:id", (req, res) => updateBorrowal(req, res))
+
+// router.delete("/delete/:id", (req, res) => deleteBorrowal(req, res))
+
+// module.exports = router;
+
 // Import required modules
-const express = require("express")
+const express = require("express");
 const router = express.Router();
 
 // Import functions from controller
+// Path ini sudah benar, memastikan nama file controller adalah 'BorrowalController.js'
 const {
     getBorrowal,
     getAllBorrowals,
     addBorrowal,
     updateBorrowal,
     deleteBorrowal
-} = require('../controllers/BorrowalController')
+} = require('../controllers/BorrowalController');
 
-router.get("/getAll", (req, res) => getAllBorrowals(req,res))
+// Penyesuaian: Langsung memanggil fungsi controllernya. Ini lebih ringkas.
+router.get("/getAll", getAllBorrowals);
 
-router.get("/get/:id", (req, res) => getBorrowal(req, res))
+router.get("/get/:id", getBorrowal);
 
-router.post("/add", (req, res) => addBorrowal(req, res))
+router.post("/add", addBorrowal);
 
-router.put("/update/:id", (req, res) => updateBorrowal(req, res))
+router.put("/update/:id", updateBorrowal);
 
-router.delete("/delete/:id", (req, res) => deleteBorrowal(req, res))
+router.delete("/delete/:id", deleteBorrowal);
 
 module.exports = router;
